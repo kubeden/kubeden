f49286917c41a8bf7ebd19931280aa2d0e754486
Kuberdenis <kuberdenis@Kuberdeniss-MacBook-Pro.local>
2024-10-21 15:15:31 +0300

    display ARGOCD AND KUBERNETES AND GITHUB WORKFLOWS

--- nextjs/markdown-dot-kubeden/src/pages/index.js
+++ nextjs/markdown-dot-kubeden/src/pages/index.js
@@ -107,7 +107,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
       <Head>
-        <title>KUBERDENIS' Markdown Editor</title>
+        <title>KUBERDENIS' Markdown Editor (GOOD)</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
